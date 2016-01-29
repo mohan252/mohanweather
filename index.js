e3f3f15f59609fa669acd88cc4c5357093394b05
mohan252 <mohan252@gmail.com>
2016-01-29 11:38:10 -0500

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.get('/test', function(request, response) {
 });
 
 app.post('/receive', function(request, response) {
-  console.log(req.body);
+  console.log("logging" + req.body);
   response.writeHeader(200, {"Content-Type": "application/json"});  
   response.write("{poststatus:ok}"); 
   response.end();
