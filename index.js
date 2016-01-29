9f7229cdeac257c0f81dfa7515c1394871998356
mohan252 <mohan252@gmail.com>
2016-01-29 12:10:44 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.set('port', (process.env.PORT || 5000));
 
