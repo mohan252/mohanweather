cc3b31f5944a0e453ccbe1044650c00b0c6512b9
mohan252 <mohan252@gmail.com>
2016-03-31 11:44:23 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var cache = require('memory-cache');
+var moment = require('moment-timezone');
+
 var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
@@ -28,8 +30,13 @@ app.get('/', function(request, response) {
 
 app.post('/photon', function(request, response) {
   var data = JSON.stringify(request.body);
-  var d = new Date();
-  var dataToBeCached = { "date": d.toLocaleString(), "data": data };
+  
+  var d = moment().tz("America/New_York").format();
+  var dataToBeCached = { "date": d, "data": data };
+  
+  // var d = new Date();
+  // var dataToBeCached = { "date": d.toLocaleString(), "data": data };
+  
   var cachedData = cache.get("data");
   if(cachedData != null){
     cachedData.push(dataToBeCached);
